d0f0846484b97bcc98e39c25076904cd7db5d7ad
smita1078 <98531258+smita1078@users.noreply.github.com>
2024-07-23 23:34:02 +0530

    Add files via upload

---
+++ Assignment-2/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import PhotoGallery from './components/PhotoGallery';
+import './styles/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PhotoGallery />
+    </div>
+  );
+}
+
+export default App;
+
+
